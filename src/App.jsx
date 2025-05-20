9a99e226a22b648917cb331ef34539605ab05658
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-19 21:36:11 -0400

    created state constants

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import List from './components/List';
 import Alert from './components/Alert';
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show:false,msg:'',type:''});
 
   return (
     <div>
