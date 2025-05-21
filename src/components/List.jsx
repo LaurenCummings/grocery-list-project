dc7519ada6a467cd0007a3e6ec77def1bddc900a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-20 23:39:32 -0400

    mapped out items passed to list with edit and delete buttons

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,22 @@ function List({items}) {
 
     return (
         <div className="grocery-list">
-            
+            {items.map((item) => {
+                const {id, title} = item;
+                return (
+                    <article key={id} className={"grocery-item"}>
+                        <p className="title">{title}</p>
+                        <div className="btn-container">
+                            <button type="button" className="edit-btn">
+                                <FaEdit />
+                            </button>
+                            <button type="button" className="delete-btn">
+                                <FaTrash />
+                            </button>
+                        </div>
+                    </article>
+                )
+            })}
         </div>
     )
 }
