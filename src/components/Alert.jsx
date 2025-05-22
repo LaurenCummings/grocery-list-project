3e160eb38b79fa72e07c6a5da5ea70cbbc0a8293
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-21 23:50:54 -0400

    Added list as dependency for alert timeout so last alert stays for 3 seconds even with overlap

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,13 +1,13 @@
 import '../css/Alert.css';
 import { useEffect } from 'react';
 
-function Alert({ type, msg, removeAlert }) {
+function Alert({ type, msg, removeAlert, list }) {
     useEffect(() => {
         const timeout = setTimeout(() => {
             removeAlert();
         }, 3000)
         return () => clearTimeout(timeout)
-    },[])
+    },[list])
 
     return (
         <p className={`alert alert-${type}`}>
