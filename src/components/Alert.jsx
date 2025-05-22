36949e4041e56997521450c73bc283d2cf235cd8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-21 23:35:58 -0400

    Added timeout to remove alert after 3 seconds

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,6 +1,13 @@
 import '../css/Alert.css';
+import { useEffect } from 'react';
 
-function Alert({ type, msg }) {
+function Alert({ type, msg, removeAlert }) {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            removeAlert();
+        }, 3000)
+        return () => clearTimeout(timeout)
+    },[])
 
     return (
         <p className={`alert alert-${type}`}>
